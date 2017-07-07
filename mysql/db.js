2973d45e59092b91c71f9194541feca21c1e47af
douyy <1253846139@qq.com>
2017-07-07 16:51:58 +0800

    豆影 分页初次提交

---
+++ mysql/db.js
@@ -0,0 +1,17 @@
+// 数据库
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'maixiangCake'
+});
+conn.connect(function(err){
+    if (err) {
+        throw err;
+        console.log('数据库连接失败');
+    }else{
+        console.log('数据库连接成功');
+    }
+});
+module.exports = conn;
