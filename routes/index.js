97ec57b541cf8ade387ef559cd3558301e855946
douyy <1253846139@qq.com>
2017-07-09 17:36:35 +0800

    豆影 接口更新

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var conn = require('../mysql/db');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
+router.get('/',function(req,res){
+    res.render('index', {
+      title: '麦香蛋糕'
+    });
+});
+router.get('/a', function(req, res, next) {
+    conn.query('select * from cake where id <= 5',function(err,user){
+        if (err) {
+            return res.send({
+                success:false,
+                data:err.message
+            });
+        }
+        return res.send({
+            success:true,
+            data:user
+        });
+    });
 });
 module.exports = router;
